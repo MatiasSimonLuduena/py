8dbb2aa7a5935908871468a8c9017d82861a39cd
MatiasSimonLuduena <matipax04@gmail.com>
2024-08-23 17:42:17 -0300

    correcciones 1

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -3,13 +3,13 @@ import "./inicio.css"
 const Inicio = () => {
   return (
     <main>
-        <h1>Iniciar Sesión</h1>
-        <div className="container_form">
-            <input type="email" placeholder="Email"/>
-            <input type="password" placeholder="Contraseña"/>
-            <button>Iniciar Sesión</button>
-        </div>
-        <p>Versión:v1.116.0</p>
+      <h1>Iniciar Sesión</h1>
+      <div className="container_form">
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Contraseña"/>
+        <button>Iniciar Sesión</button>
+      </div>
+      <p>Versión:v1.116.0</p>
     </main>
   )
 }
